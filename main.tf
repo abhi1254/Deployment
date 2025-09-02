116d1a54f14669919277f0b1931155aa24f5c470
Abhishek Ojha <51531951+abhi1254@users.noreply.github.com>
2025-09-02 12:09:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 module "rg" {
-  source              = "../Child/modules/resource_group"
+  source              = "./Child/resource_group"
   resource_group_name = var.resource_group_name
   location            = var.location
 }
 
 module "vnet" {
-  source              = "../Child/modules/virtual_network"
+  source              = "./Child/virtual_network"
   vnet_name           = var.vnet_name
   location            = module.rg.location
   resource_group_name = module.rg.name
 }
 
 module "subnet" {
-  source              = "../Child/modules/subnet"
+  source              = "./Child/subnet"
   subnet_name         = var.subnet_name
   resource_group_name = module.rg.name
   vnet_name           = module.vnet.name
 }
 
 module "vm" {
-  source              = "../Child/modules/virtual_machine"
+  source              = "./Child/virtual_machine"
   vm_name             = var.vm_name
   resource_group_name = module.rg.name
   location            = module.rg.location
