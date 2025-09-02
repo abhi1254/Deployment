1727f98b115c1619d46f6fd73725c76a97e5ab88
Abhishek Ojha <51531951+abhi1254@users.noreply.github.com>
2025-09-02 14:05:39 +0530

    Update Main.tf

--- Child/virtual_machine/Main.tf
+++ Child/virtual_machine/Main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "this" {
   name                = "${var.vm_name}-pip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "this" {
